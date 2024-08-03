4e73f3d1ace816dcd72d23143b48b43d60842588
MarceloHaSilva <marcelohenriqueas17@gmail.com>
2024-08-03 19:52:13 -0300

    Finalized

--- weather-conditions-jsVanilla/js/index.js
+++ weather-conditions-jsVanilla/js/index.js
@@ -10,22 +10,35 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const form = document.querySelector("#search-form > form");
 const input = document.querySelector("#input-location");
+const sectionWeatherInfo = document.querySelector("#weather-info");
 form === null || form === void 0 ? void 0 : form.addEventListener("submit", (event) => __awaiter(void 0, void 0, void 0, function* () {
     event.preventDefault();
-    console.log("submit");
-    if (!input)
+    if (!input || !sectionWeatherInfo)
         return;
     const location = input.value;
     if (location.length < 3) {
-        alert('The place must have, at least, 3 letters!');
+        alert("The place must have, at least, 3 letters!");
         return;
     }
-    const response = yield fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5ca2c6bf50125fd5847b6b5a06f1c070&lang=en_br&units=metric`);
-    const data = yield response.json();
-    console.log(data);
-    const infos = {
-        temperature: Math.round(data.main.temp),
-        local: data.name,
-        icon: `https://openweathermap.org/img/wn/${data.weather.icon}/@2x.png`
-    };
+    try {
+        const response = yield fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5ca2c6bf50125fd5847b6b5a06f1c070&lang=en_br&units=metric`);
+        const data = yield response.json();
+        const infos = {
+            temperature: Math.round(data.main.temp),
+            local: data.name,
+            icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+        };
+        sectionWeatherInfo.innerHTML = `
+    <div class="weather-data">
+          <h2>${infos.local}</h2>
+  
+          <span>${infos.temperature}°C</span>
+     </div>
+  
+      <img src="${infos.icon}" alt="">
+    `;
+    }
+    catch (err) {
+        console.log("Gave an error in getting the data from the API", err);
+    }
 }));
--- weather-conditions-jsVanilla/ts/index.ts
+++ weather-conditions-jsVanilla/ts/index.ts
@@ -2,27 +2,41 @@ const form = document.querySelector("#search-form > form");
 const input: HTMLInputElement | null =
   document.querySelector("#input-location");
 
+const sectionWeatherInfo = document.querySelector("#weather-info");
+
 form?.addEventListener("submit", async (event) => {
   event.preventDefault();
-  console.log("submit");
 
-  if (!input) return;
+  if (!input || !sectionWeatherInfo) return;
   const location = input.value;
 
-  if(location.length < 3) {
-    alert('The place must have, at least, 3 letters!');
+  if (location.length < 3) {
+    alert("The place must have, at least, 3 letters!");
     return;
   }
 
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5ca2c6bf50125fd5847b6b5a06f1c070&lang=en_br&units=metric`)
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5ca2c6bf50125fd5847b6b5a06f1c070&lang=en_br&units=metric`
+    );
+    const data = await response.json();
 
-  console.log(data);
+    const infos = {
+      temperature: Math.round(data.main.temp),
+      local: data.name,
+      icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+    };
+
+    sectionWeatherInfo.innerHTML = `
+    <div class="weather-data">
+          <h2>${infos.local}</h2>
   
-
-  const infos = {
-    temperature : Math.round(data.main.temp),
-    local : data.name,
-    icon : `https://openweathermap.org/img/wn/${data.weather.icon}/@2x.png`
+          <span>${infos.temperature}°C</span>
+     </div>
+  
+      <img src="${infos.icon}" alt="">
+    `;
+  } catch (err) {
+    console.log("Gave an error in getting the data from the API", err);
   }
 });
